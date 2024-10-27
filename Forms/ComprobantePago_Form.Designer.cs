1f850420dc15b0d23746379e78e98b8ff6f7ea1f
MartoJuanDev <martojuandev@gmail.com>
2024-10-27 19:24:30 -0300

    agrego el form para armar luego comprobante de pago

---
+++ Forms/ComprobantePago_Form.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace DSOO_Grupo4_TP1.Forms
+{
+    partial class ComprobantePago_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // ComprobantePago_Form
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1523, 678);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ComprobantePago_Form";
+            Text = "ComprobantePago_Form";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
